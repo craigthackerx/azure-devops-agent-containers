43ef091dc711775bc7d5875b3b88aa47b8b5de68
Craig Thacker <craigthackerx@gmail.com>
2022-01-23 17:43:29 +0000

    add windows server workflow

---
+++ devops-agents/windows/server/base/docker-build.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh
+
+Set-PSDebug -Trace 1
+
+
+$REGISTRY = "ghcr.io"
+$USER = "craigthackerx"
+$IMAGE_NAME = "azure-devops-agent-base-win20h2:"
+$TAGS = "latest"
+$DOCKERFILE_NAME = "Base.Dockerfile"
+
+$NORMAL_USER = "ContainerAdministrator"
+$PYTHON3_VERSION = "@latest"
+
+  docker build `
+    --file=$DOCKERFILE_NAME `
+    --tag=$REGISTRY/$USER/$IMAGE_NAME$TAGS `
+    --build-arg ACCEPT_EULA=y `
+    --build-arg NORMAL_USER=$NORMAL_USER `
+    --build-arg PYTHON3_VERSION=$PYTHON3_VERSION `
+    .
